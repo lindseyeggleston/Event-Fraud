b84e819328ebbe18a7461ae122298ba7a9b2e84a
TimKahn <kahn.tim@gmail.com>
2017-10-06 11:41:22 -0600

    created data clean script

---
+++ code/tim_so-fresh-so-clean.py
@@ -0,0 +1,10 @@
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+
+def prep_data(filename):
+    df = data = pd.read_json()
+    return df
+
+
+if __name__ == '__main__':
+    df = prep_data('../data/data.json')
