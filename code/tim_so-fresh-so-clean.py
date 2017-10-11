1db43f990f93777d65c75ce6f0fa8dffb9170616
TimKahn <kahn.tim@gmail.com>
2017-10-11 08:59:09 -0600

    initial data prep

--- code/tim_so-fresh-so-clean.py
+++ code/tim_so-fresh-so-clean.py
@@ -1,10 +1,12 @@
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
 
 def prep_data(filename):
-    df = data = pd.read_json()
+    df = pd.read_json(filename)
+    df['fraud'] = df['acct_type'].apply(lambda x: 1 if 'fraud' in x else 0)
+    df.drop('acct_type', axis=1, inplace=True)
     return df
-
 
 if __name__ == '__main__':
     df = prep_data('../data/data.json')
