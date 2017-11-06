48807777f104e9255aa326851d9bb8120333b1ac
TimKahn <kahn.tim@gmail.com>
2017-11-06 09:21:15 -0700

    model eval

--- model_evaluation.py
+++ model_evaluation.py
@@ -8,11 +8,16 @@ from xgboost import XGBClassifier
 def preprocess(filename):
     X = pd.read_pickle(filename)
     y = X.pop('fraud').values
-    numeric_columns = ['body_length', 'name_length', 'num_order', 'num_payouts', 'sale_duration2', 'user_age']
+    numeric_columns = ['body_length', 'name_length', 'num_order', 'num_payouts', 'sale_duration2', 'user_age', 'event_duration', 'creation_duration']
     ss = StandardScaler()
     X[numeric_columns] = ss.fit_transform(X[numeric_columns])
+    X = X[numeric_columns] #shortcurt, we need to add categorical variables!!!
     return train_test_split(X, y, test_size=.1, random_state=42, stratify=y)
 
 if __name__ == '__main__':
     X_train, X_test, y_train, y_test = preprocess('data/clean_data2.pkl')
-    xg = XGBClassifier(scale_pos_weight=10, max_delta_step=1, colsample_bytree=.5, colsample_bylevel=.8, seed=42)
+    xg1 = XGBClassifier(seed=42)
+    xg2 = XGBClassifier(scale_pos_weight=10, max_delta_step=1, colsample_bytree=.5, colsample_bylevel=.8, seed=42)
+    classifiers = [xg1, xg2]
+    balancing = []
+    roc.plot_ROC_curve(classifiers, X, y, balancing=balancing, save_path=None)
