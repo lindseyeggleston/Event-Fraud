4b84f9d33fc4ee1179f9226b38044e836788761a
Lindsey Eggleston <lindsey.eggleston@gmail.com>
2017-11-13 16:32:40 -0700

    add text to cover

--- web_app/app.py
+++ web_app/app.py
@@ -1,10 +1,10 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 app = Flask(__name__)
 
 # Home page
 @app.route('/')
 def cover():
-    pass
+    return render_template('cover_index.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
