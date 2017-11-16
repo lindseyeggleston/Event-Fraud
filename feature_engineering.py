4a472e3db8bdde397b2d01a4a6799965bbecc3d4
Lindsey Eggleston <lindsey.eggleston@gmail.com>
2017-11-16 14:32:51 -0700

    add docstrings to all functions

--- feature_engineering.py
+++ feature_engineering.py
@@ -54,6 +54,10 @@ def create_duration_cols(df, cols, time_unit='days'):
 
 
 def _ticket_spread(lst):
+    '''
+    Calculates the price difference between the most and least expensive ticket
+    types
+    '''
     cost = set()
     for ticket in lst:
         cost.add(ticket['cost'])
@@ -64,6 +68,9 @@ def _ticket_spread(lst):
 
 
 def _avg_ticket_price(lst):
+    '''
+    Calculated the average ticket price
+    '''
     cost = []
     num_tickets = []
     price = 0
@@ -76,6 +83,10 @@ def _avg_ticket_price(lst):
 
 
 def _percent_tickets_sold(lst):
+    '''
+    Returns the percentage of tickets sold out of total available
+    '''
+
     sold = 0
     total = 0
     for ticket in lst:
@@ -88,14 +99,35 @@ def _percent_tickets_sold(lst):
 
 
 def extract_ticket_info(df, col='ticket_types', drop_col=False):
+    '''
+    Extract ticket information such as the spread between ticket price values,
+    number of ticket types, and the average ticket price
+
+    Inputs
+    ------
+    df: pandas DataFrame
+    col: STR - (default = 'ticket_types') name of column containing ticket
+        information
+    drop_col: BOOL - if True, removes 'col' from df
+
+    Output
+    ------
+    pandas DataFrame
+    '''
+
     df['num_ticket_types'] = df[col].apply(lambda x: len(x))
     df['price_spread'] = df[col].apply(lambda x: _ticket_spread(x))
     df['avg_ticket_price'] = df[col].apply(lambda x: _percent_tickets_sold(x))
 
     if drop_col == True:
         df.drop(col, axis=1, inplace=True)
+    return df
 
 def _extract_text(st):
+    '''
+    Extract text from <p> tags in html string
+    '''
+
     soup = BeautifulSoup(st,'html.parser')
     p = soup.find_all('p')
     text = ''
@@ -103,8 +135,13 @@ def _extract_text(st):
         text += i.text
     return text
 
-def _unique_words(st):
-    words = st.split()
+def _unique_words(text):
+    '''
+    Creates dictionary of unique words (keys) and their occurances (values)
+    in text
+    '''
+
+    words = text.split()
     word_dic = {(word, 0) for word in set(words)}
     for word in words:
         word_dic[word] += 1
@@ -113,7 +150,8 @@ def _unique_words(st):
 def extract_description_text(df, description_col, unique_words=False,
         drop_col=False):
     '''
-    des
+    Extracts text data from description_col and engineers new columns related to
+    that text.
 
     Inputs
     ------
@@ -125,8 +163,7 @@ def extract_description_text(df, description_col, unique_words=False,
 
     Outputs
     -------
-    pandas DataFrame with new columns 'text', 'text_length', and 'nunique_words'
-    (and potentially 'unique_words')
+    pandas DataFrame
     '''
 
     temp = [_extract_text(entry) for entry in df[description_col]]
@@ -148,6 +185,6 @@ if __name__ == '__main__':
     dur_cols = {'event_duration':['event_start','event_end'], 'user_duration':
             ['user_created','event_created']}
     df = create_duration_cols(df, dur_cols)
-    extract_ticket_info(df, drop_col=True)
+    df = extract_ticket_info(df, drop_col=True)
     df = extract_description_text(df, 'description')
     df.to_pickle('data/clean_data3.pkl')
